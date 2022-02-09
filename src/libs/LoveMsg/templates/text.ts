71d49007fecca2f253facfd3fb09adf831dbb9ec
1131153523@qq.com <fanyihui@tuhu.cn>
2022-02-09 18:19:58 +0800

    changes

---
+++ src/libs/LoveMsg/templates/text.ts
@@ -0,0 +1,83 @@
+/*
+ * @Author: fanyihui
+ * @Date: 2022-02-05 19:31:39
+ * @LastEditors: VSCode
+ * @LastEditTime: 2022-02-05 21:29:25
+ * @FilePath: /notify-server/src/libs/LoveMsg/templates/text.ts
+ * @Description:
+ *
+ * Copyright (c) 2022 by fanyihui/tuhu, All Rights Reserved.
+ */
+/**
+ * @description 纯文本模板-企业微信消息通知
+ * https://open.work.weixin.qq.com/api/doc/90000/90135/90236
+ */
+
+import dayjs, { weekToday } from '../../../utils/dayjs'
+
+export const textTemplate = (data: TextTemplateProps) => {
+  const { caiHongpi, sayLove, songLyrics, oneMagazines, netEaseCloud, oneWord, dayEnglish } = data
+
+  let text = '早安呀，我可爱的明悦~\n'
+
+  // 工作日/休息日，需要排除节假日
+  const week = weekToday()
+  if (['星期六', '星期日'].includes(week)) {
+    text += `
+我已经起床啦！向你说早安呦~，记得吃早饭呀😆\n
+嗯哼哼~今天可是${week}哦，上班别迟到了哦~`
+  }
+  else {
+    text += `
+我还没起床呀！就等着小明悦起床给我说早安呦🤣
+嗯哼~，既然今天是${week}，就让你再睡会懒觉~下次可不能啦~😝\n`
+  }
+
+  // 添加笑话
+  if (caiHongpi) {
+    //     text += `
+    // 彩虹屁：
+    text += `
+${caiHongpi.content}\n`
+  }
+
+  if (sayLove) {
+    text += `
+${sayLove.content}\n`
+  }
+
+  // 诗句
+  if (songLyrics) {
+    text += `
+『${songLyrics.source}』${songLyrics.content}\n`
+  }
+
+  if (oneMagazines) {
+    text += `
+『ONE杂志』${oneMagazines.word}\n`
+  }
+
+  if (netEaseCloud) {
+    text += `
+『网易云音乐热评』${netEaseCloud.content}——${netEaseCloud.source}\n`
+  }
+
+  // 添加一句一言
+  if (oneWord) {
+    text += `
+『一言』${oneWord.hitokoto}\n`
+  }
+
+  // 每日英语
+  if (dayEnglish) {
+    text += `
+『每日英语（${dayjs(dayEnglish.date).format('ll')}』${dayEnglish.content}`
+  }
+
+  return {
+    msgtype: 'text',
+    text: {
+      content: text,
+    },
+  }
+}
---
+++ src/libs/LoveMsg/templates/textcard.ts
@@ -0,0 +1,108 @@
+/*
+ * @Author: fanyihui
+ * @Date: 2022-02-05 19:31:39
+ * @LastEditors: VSCode
+ * @LastEditTime: 2022-02-05 21:28:41
+ * @FilePath: /notify-server/src/libs/LoveMsg/templates/textcard.ts
+ * @Description:
+ *
+ * Copyright (c) 2022 by fanyihui/tuhu, All Rights Reserved.
+ */
+/**
+ * @description 文本卡片模板 title + description
+ * https://open.work.weixin.qq.com/api/doc/90000/90135/90236
+ */
+
+/**
+ * 卡片类型模板定义
+ * 模板内容配置
+ * 微信通知 textcard类型的description内容限制512个字节
+ */
+
+import dayjs from '../../../utils/dayjs'
+
+// 相识的日子
+const start_stamp = '2022-01-16'
+
+export const textCardTemplate = (data: TextCardTemplateProps) => {
+  const {
+    area,
+    date,
+    weather,
+    highest,
+    lowest,
+    wind,
+    windsc,
+    humidity,
+    week,
+    pop,
+    pcpn,
+    tips,
+    lunarInfo,
+  } = data
+
+  // 今日、恋爱天数
+  const today = `${date.replace('-', '年').replace('-', '月')}日`
+  const dateLength = dayjs(date).diff(start_stamp, 'day')
+
+  // 公历节日、农历节日和二十四节气
+  const { festival, lunar_festival, jieqi, lubarmonth, lunarday } = lunarInfo
+  const festival_info = festival ? `| ${festival}` : ''
+  const lunar_festival_info = lunar_festival ? `| ${lunar_festival}` : ''
+  const jieqi_info = jieqi ? `| ${jieqi}` : ''
+
+  // 拼接内容
+  let description = `${area} | ${today} | ${week} ${festival_info}
+农历 | ${lubarmonth}${lunarday} ${lunar_festival_info} ${jieqi_info}\n
+今日天气状况：
+天气：${weather}
+${wind}：${windsc}
+温度：${lowest}℃ ~ ${highest}℃
+湿度：${humidity}\n`
+
+  if (weather.includes('雨')) {
+    description += `降雨概率：${pop}%
+降雨量：${pcpn}mm\n`
+  }
+  // 生活指数提示
+  if (tips) {
+    description += `
+${tips}\n`
+  }
+
+  // 最高温度
+  if (+highest <= 3) {
+    description += `
+哈喽哈喽~这里是来自崽崽的爱心提醒哦：
+今日最高温度仅为🥶 ${highest}℃，可冷可冷了~
+鱼崽崽可要注意保暖哦~\n`
+  }
+
+  //   if (air_tips) {
+  //     description += `
+  // 出行建议：${air_tips}`
+  //   }
+
+  //   if (oneWord) {
+  //     description += `
+  // 『 ${oneWord.hitokoto} 』`
+  //   }
+
+  // 内容末尾，自定义
+  description += `
+  [ 点我有惊喜 ] ❤️ 🧡 💛 💚 💖`
+
+  const title = `这是我们相识的第 ${dateLength} 天`
+
+  return {
+    msgtype: 'textcard',
+    textcard: {
+      title,
+      description,
+      //   url: 'https://api.lovelive.tools/api/SweetNothings',
+      //   url: 'https://v1.jinrishici.com/all.svg',
+      url: 'https://api.vvhan.com/api/60s', // 60s看世界
+      btntxt: 'By Fyh',
+    },
+  }
+}
